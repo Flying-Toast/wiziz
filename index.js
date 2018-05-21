b1df253682469cf318f17862a83d92f8c4254ac0
Your Name <you@example.com>
2018-05-21 11:33:10 -0400

    remove spells module and put spells back in index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const server = require('./server.js');
 const helpers = require('./helpers.js');
-const spellEnts = require('./spells.js');
 const xss = require('xss');
 const readline = require('readline');
 const hashmap = require('hashmap');
@@ -39,6 +38,54 @@ var config = {
     level2: {
       newSpells: ['freezeSpell', 'blindSpell'] //which new spells are unlocked at this level
     }
+  }
+};
+var spellEnts = { //spell entities
+  fireSpell: {
+    name: 'fireSpell',
+    speed: 1,
+    range: 700,
+    radius: 10,
+    type: 'projectile',
+    effect: function(affectedPlayer) {
+      affectedPlayer.health -= 100;
+    }
+  },
+  freezeSpell: {
+    name: 'freezeSpell',
+    speed: 0.7,
+    range: 500,
+    radius: 10,
+    effectWearOff: 3000,
+    type: 'projectile',
+    effect: function(affectedPlayer) {
+      if (affectedPlayer.movementSpeed !== 0) {
+        affectedPlayer.movementSpeed = 0;
+        setTimeout(function() {
+          affectedPlayer.movementSpeed = config.playerSpeed;
+        }, this.effectWearOff);
+      }
+    }
+  },
+  blindSpell: {
+    name: 'blindSpell',
+    speed: 0.7,
+    range: 700,
+    type: 'splash',
+    explosionRadius: 140,
+    ttl: 4000,
+    radius: 10,
+    effectWearOff: 2000,
+    playerCoolDown: 100, //delay between repetitions of effectArea affecting a certain player
+    effect: function(affectedPlayer) { //effect of explosion area
+      if (!affectedPlayer.blinded) {
+        affectedPlayer.blinded = true;
+        setTimeout(function() {
+          affectedPlayer.blinded = false;
+        }, this.effectWearOff);
+      }
+    },
+    color: 'rgba(0, 0, 0, 0.6)'
   }
 };
 var spells = { //inventory items
