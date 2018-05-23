19ffa1bd2b4f9b35d50e70df2b43057628f453cc
q <q>
2018-05-23 18:37:53 -0400

    created wiki (not finished yet)

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ var server = app.listen(port, function() {
   console.log('listening on port ' + port);
 });
 
+app.get('/legal', function(req, res) {
+  res.sendFile(__dirname + '/public/legal.html');
+});
+
+app.get('/credits', function(req, res) {
+  res.sendFile(__dirname + '/public/credits.txt');
+});
+
+app.get('/wiki', function(req, res) {
+  res.sendFile(__dirname + '/public/wiki/wiki.html');
+});
+
 app.use('/', express.static('public'));
 
 exports.io = socket(server);
