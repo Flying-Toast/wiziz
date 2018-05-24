8bf885061fcf7f6e330846923e1d4621eb31350a
q <q>
2018-05-23 22:44:41 -0400

    added spell suggestion system

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const socket = require('socket.io');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 var exports = module.exports;
 
@@ -9,6 +11,24 @@ var port = process.env.PORT || '80';
 
 var server = app.listen(port, function() {
   console.log('listening on port ' + port);
+});
+
+app.get('/spellSuggestion', function(req, res) {
+  res.sendFile(__dirname + '/public/suggestSpell.html');
+});
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.post('/spellSuggestion', function(req, res) {
+
+  fs.appendFile('suggestions.txt', '\nSpell Name: ' + req.body.spellName + '\nSpell Type: ' + req.body.spellType + '\nEffect: ' + req.body.effect + '\nRequester Ip: ' + req.ip + '\nRequest Headers: ' + JSON.stringify(req.headers) + '\n=====', function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  res.redirect(303, '/thanks.html');
 });
 
 app.get('/credits', function(req, res) {
