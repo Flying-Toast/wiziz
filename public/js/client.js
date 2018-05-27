9b51ed2ace685f92526d2003cfe0d62296cc12c8
q <q>
2018-05-26 22:19:32 -0400

    toggleable instructions div on mainscreen

--- public/js/client.js
+++ public/js/client.js
@@ -312,6 +312,7 @@ socket.on('update', function(updatedGame) {
 
 onload = function() {
   document.querySelector('#loading').style.display = 'none';
+  document.querySelector('#instructionsToggle').checked = false;
 };
 
 function fillInventory(inventory) {
