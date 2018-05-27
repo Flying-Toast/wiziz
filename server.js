98f70166f481d43c400ebcc5340708149929d30f
q <q>
2018-05-26 20:53:43 -0400

    worked on mainScreen Styling, added reddit link

--- server.js
+++ server.js
@@ -24,10 +24,6 @@ var server = app.listen(port, function() {
   console.log('listening on port ' + port);
 });
 
-app.get('/tutorial', function(req, res) {
-  res.sendFile(__dirname + '/public/tutorial.html');
-});
-
 app.get('/credits', function(req, res) {
   res.sendFile(__dirname + '/public/credits.txt');
 });
