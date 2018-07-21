3a76bc27418cdb0bf411996fa8df9de1df2b7d43
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2018-07-21 17:08:28 -0400

    server sends 404 page

--- server.js
+++ server.js
@@ -42,4 +42,9 @@ app.get('/spellList', function(req, res) {
 
 app.use('/', express.static('public'));
 
+app.get('*', function(req, res) {
+  res.status(404);
+  res.sendFile(__dirname + '/public/404.html');
+});
+
 exports.io = socket(server);
