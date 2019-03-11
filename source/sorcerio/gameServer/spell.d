db39c9c3caf3dbbccd6e4ad559938b0037fba23e
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-03-11 17:44:44 -0400

    spell factory

--- source/sorcerio/gameServer/spell.d
+++ source/sorcerio/gameServer/spell.d
@@ -1,6 +1,8 @@
 module sorcerio.gameServer.spell;
 
-enum SpellName {
+import sorcerio.gameServer.server;
+
+enum SpellName {//TODO: use SpellName to generate public/spellTypes.json
 	fire,
 	cannon,
 	slow,
@@ -12,4 +14,40 @@ enum SpellName {
 	teleport,
 	speed,
 	shock
+}
+
+final class SpellFactory {
+	private static Spell[SpellName] registry;
+
+	static void registerSpell(SpellName name, Spell spell) {
+		if (name !in registry) {
+			registry[name] = spell;
+		} else {
+			throw new Exception("Spell already registered");
+		}
+	}
+
+	static Spell createSpell(SpellName name) {//add other constructor args here if needed
+		return registry[name].create();
+	}
+}
+
+
+mixin template registerSpell(SpellName name) {
+	static this() {
+		SpellFactory.registerSpell(name, new typeof(this));
+	}
+}
+
+mixin template spellCreateMethod() {
+	override Spell create() {
+		return new typeof(this);
+	}
+}
+
+
+abstract class Spell {
+	abstract Spell create();
+
+	abstract void tick(Server gameState);
 }
