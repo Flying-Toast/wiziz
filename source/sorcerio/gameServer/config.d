6e97d54ca4aa61d2de874cabf0220ac80c3025cc
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-03-12 15:52:46 -0400

    make CONFIG enums

--- source/sorcerio/gameServer/config.d
+++ source/sorcerio/gameServer/config.d
@@ -3,16 +3,16 @@ module sorcerio.gameServer.config;
 import sorcerio;
 
 immutable struct CONFIG {
-	static string defaultNickname = "Unnamed Sorcerer";
-	static ubyte maxNameLength = 15;
+	static enum string defaultNickname = "Unnamed Sorcerer";
+	static enum ubyte maxNameLength = 15;
 
-	static float defaultPlayerSpeed = 1.0 / 4.5;///the default player speed in pixels/millisecond
+	static enum float defaultPlayerSpeed = 1.0 / 4.5;///the default player speed in pixels/millisecond
 
-	static ushort maxPlayers = 100;///the maximum players allowed in a single Server
+	static enum ushort maxPlayers = 100;///the maximum players allowed in a single Server
 
-	static int minMapSize = 3000;
-	static int maxMapSize = 20000;
-	
-	static long updateInterval = 100;///the delay, in milliseconds, between updates being sent to clients
-	static long physicsInterval = 10;///the delay, in milliseconds between iterations of the physics loop
+	static enum int minMapSize = 3000;
+	static enum int maxMapSize = 20000;
+
+	static enum long updateInterval = 100;///the delay, in milliseconds, between updates being sent to clients
+	static enum long physicsInterval = 10;///the delay, in milliseconds between iterations of the physics loop
 }
