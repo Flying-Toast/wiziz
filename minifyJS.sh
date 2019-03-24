6d08e240d22e419db6084be9fec5c78a2c5acd74
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-03-24 16:56:32 -0400

    add --clean option to minifyJS.sh

--- minifyJS.sh
+++ minifyJS.sh
@@ -5,6 +5,10 @@ if [[ ! $(which terser) ]]; then
 	exit
 fi
 
-terser public/js/client.js --compress --mangle ---mangle-props keep_quoted --enclose --output public/js/client.js
-
-echo "minified public/js/client.js. Remember not to check it in!"
+if [[ $1 == "--clean" ]]; then
+	git checkout -- public/js/client.js
+	echo "Un-minified public/js/client.js. It is now okay to commit to git."
+else
+	terser public/js/client.js --compress --mangle ---mangle-props keep_quoted --enclose --output public/js/client.js
+	printf "Minified public/js/client.js. \033[1;31mRemember to not check it in!\033[0m\n"
+fi
