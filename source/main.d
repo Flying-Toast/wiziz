c5416383c5c5b60896c5ce290a696910188d5209
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-03-24 12:00:35 -0400

    create messageQueue in main()

--- source/main.d
+++ source/main.d
@@ -1,7 +1,9 @@
 import sorcerio;
+import sorcerio.webServer.messageQueue;
 import std.concurrency;
 
 void main() {
+	shared queue = new MessageQueue;
 	Tid gameServerTid = spawn(&startGameServer);
 
 	startWebServer(8080u, gameServerTid);
