c5a3b7a5812a71451865813b627715fd47693425
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-03-24 11:55:28 -0400

    messageQueue class

---
+++ source/sorcerio/webServer/messageQueue.d
@@ -0,0 +1,24 @@
+module sorcerio.webServer.messageQueue;
+
+shared class MessageQueue {
+	private string[][uint] queue;
+
+	///checks if there is a message available in the queue for `socketId`.
+	bool messageAvailable(uint socketId) {
+		return (socketId in queue) && queue[socketId].length > 0;
+	}
+
+	///appends `message` to the queue for `socketId`.
+	void queueMessage(uint socketId, string message) {
+		queue[socketId] ~= message;
+	}
+
+	///pops the next message from `socketId` off the queue and returns it.
+	string nextMessage(uint socketId) {
+		import std.algorithm.mutation;
+
+		string message = queue[socketId][0];
+		queue[socketId] = queue[socketId].remove(0);
+		return message;
+	}
+}
