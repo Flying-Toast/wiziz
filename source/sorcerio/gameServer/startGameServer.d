43e28fb8247e806df9da0ad62bfefd9a873c0a10
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-04-08 20:10:28 -0400

    CI fix

--- source/sorcerio/gameServer/startGameServer.d
+++ source/sorcerio/gameServer/startGameServer.d
@@ -11,27 +11,38 @@ import core.thread;
 void startGameServer(shared MessageQueue queue) {
 	version (Posix) {
 		scope (exit) {
-			version (dubTest) {//for CI - don't fail when owner thread terminates
-				import core.stdc.stdlib;
-				_Exit(EXIT_SUCCESS);
-			} else {//normal behavior:
-				import core.stdc.stdlib;
-				_Exit(EXIT_FAILURE);
-			}
+			import core.stdc.stdlib;
+			_Exit(EXIT_FAILURE);
 		}
 	}
 
 	ServerManager master = new ServerManager(queue);
 
 	while (true) {
-		receiveTimeout(Duration.zero,
-			(shared PlayerConfig cfg) {
-				master.addPlayerToServer(cast(PlayerConfig) cfg);
-			},
-			(uint disconnectSocketId) {
-				master.removePlayerBySocketId(disconnectSocketId);
+		version (dubTest) {//for CI - don't fail if owner thread terminates
+			try {
+				receiveTimeout(Duration.zero,
+					(shared PlayerConfig cfg) {
+						master.addPlayerToServer(cast(PlayerConfig) cfg);
+					},
+					(uint disconnectSocketId) {
+						master.removePlayerBySocketId(disconnectSocketId);
+					}
+				);
+			} catch (OwnerTerminated e) {
+				import core.stdc.stdlib;
+				_Exit(EXIT_SUCCESS);
 			}
-		);
+		} else {//normal behavior:
+			receiveTimeout(Duration.zero,
+				(shared PlayerConfig cfg) {
+					master.addPlayerToServer(cast(PlayerConfig) cfg);
+				},
+				(uint disconnectSocketId) {
+					master.removePlayerBySocketId(disconnectSocketId);
+				}
+			);
+		}
 
 		master.tick();
 		Thread.sleep(dur!"msecs"(5));
