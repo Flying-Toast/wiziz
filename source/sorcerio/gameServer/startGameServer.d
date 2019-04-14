158384286218f9dad8b1c78484995718e31b698a
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-04-14 16:48:08 -0400

    make sure all spells are implemented during unittest

--- source/sorcerio/gameServer/startGameServer.d
+++ source/sorcerio/gameServer/startGameServer.d
@@ -16,6 +16,56 @@ void startGameServer(shared MessageQueue queue) {
 		}
 	}
 
+	version (dubTest) {//make sure that all spells are implemented
+		import std.traits;
+		import core.exception;
+		import std.stdio;
+		import std.conv;
+
+		bool error = false;
+		foreach (name; [EnumMembers!SpellName]) {
+			//make sure that all spells in are registered:
+			try {
+				SpellFactory.getCoolDownTime(name);
+			} catch (RangeError e) {
+				writeln("Spell not registered with SpellFactory: ", name);
+				//error = true; TODO: uncomment this once all spells are implemented
+			}
+
+			//make sure that all spells have images for their inventory slot:
+			bool fileExists(string path) {
+				import std.file;
+				if (path.exists && path.isFile) {
+					return true;
+				}
+				return false;
+			}
+
+			string inventoryItemPath = "public/media/images/" ~ name.to!string ~ "Spell.png";
+			if (!fileExists(inventoryItemPath)) {
+				writeln("'", name.to!string, "' spell does not have an inventory image at ", inventoryItemPath);
+				error = true;
+			}
+
+			//make sure that all spells have sounds:
+			string baseSoundPath = "public/media/sounds/" ~ name.to!string ~ "Spell.";
+			if (!fileExists(baseSoundPath~"ogg")) {
+				writeln("'", name.to!string, "' spell does not have a sound at ", baseSoundPath~"ogg");
+				error = true;
+			}
+
+			if (!fileExists(baseSoundPath~"mp3")) {
+				writeln("'", name.to!string, "' spell does not have a sound at ", baseSoundPath~"mp3");
+				error = true;
+			}
+		}
+
+		if (error) {
+			writeln("Not all spells defined in SpellName are fully implemented (see above messages).");
+			throw new Exception("Not all spells defined in SpellName are fully implemented (see above messages).");
+		}
+	}
+
 	ServerManager master = new ServerManager(queue);
 
 	while (true) {
