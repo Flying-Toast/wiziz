cff92f2379eaa007a38a740e4763240d271c8b92
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-04-25 11:10:37 -0400

    BombSpell

--- source/sorcerio/gameServer/spells.d
+++ source/sorcerio/gameServer/spells.d
@@ -240,3 +240,29 @@ class HealSpell : SelfSpell {
 		caster.health += healthDiff * healPercent / 100;
 	}
 }
+
+class BombSpell : SplashSpell {
+	mixin registerSpell!(SpellName.bomb, 2300);
+
+	private ushort damage = 100;
+
+	override void initialize() {
+		speed = 0.5;
+		range = 1500;
+		radius = 10;
+		explosionRadius = 300;
+		color = "#E80606";
+		explosionTTL = 700;
+		effectDelay = cast(ushort) (explosionTTL + 1);//set this to longer than explosionTTL so that it will only affect a given player once
+		super.initialize();
+	}
+
+	override string humanReadableEffect() {
+		import std.conv;
+		return "Affected players get -" ~ damage.to!string ~ " health";
+	}
+
+	override void splashAffect(Player player) {
+		player.doDamage(damage);
+	}
+}
