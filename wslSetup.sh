4ae23301b10880562e187f76d499bfb3c4275fbf
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-05-25 09:32:11 -0400

    update wslSetup.sh

--- wslSetup.sh
+++ wslSetup.sh
@@ -5,20 +5,18 @@
 echo "Setting up development environment. This may take a while..."
 
 # install dependencies:
-sudo apt-get update
-sudo apt-get install man-db -y # Hangs on 'processing triggers for man-db' without this for some reason.
+sudo apt-get update > /dev/null
+sudo apt-get install man-db -y > /dev/null # Hangs on 'processing triggers for man-db' without this for some reason.
 echo "installed man-db."
-sudo apt-get install libssl-dev -y
+sudo apt-get install libssl-dev -y > /dev/null
 echo "installed libssl."
-sudo apt-get install libcrypto++-dev -y
+sudo apt-get install libcrypto++-dev -y > /dev/null
 echo "installed libcrypto."
-sudo apt-get install gcc -y
+sudo apt-get install gcc -y > /dev/null
 echo "installed gcc."
 
 # install D:
-OLD_PROMPT=$PS1
 echo "source $(curl -fsS https://dlang.org/install.sh | bash -s dmd --activate)" >> ~/.bashrc
-echo "PS1=\"$OLD_PROMPT\"" >> ~/.bashrc
 source ~/.bashrc
 
 echo "Done!"
