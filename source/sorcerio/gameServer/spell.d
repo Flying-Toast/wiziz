e2b777a5fd1f35557098f8f5b39473173073c2c5
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-06-02 14:34:49 -0400

    snipe spell

--- source/sorcerio/gameServer/spell.d
+++ source/sorcerio/gameServer/spell.d
@@ -23,7 +23,8 @@ enum SpellName {
 	expansion,
 	nuke,
 	confuse,
-	repel
+	repel,
+	snipe
 }
 
 /**
@@ -42,7 +43,8 @@ private enum SpellName[][ushort] spellUnlocks = [
 	5: [SpellName.expansion, SpellName.freeze],
 	6: [SpellName.teleport, SpellName.shock],
 	7: [SpellName.invisible, SpellName.nuke],
-	8: [SpellName.confuse, SpellName.repel]
+	8: [SpellName.confuse, SpellName.repel],
+	9: [SpellName.snipe]
 ];
 
 version (unittest) {
---
+++ source/sorcerio/gameServer/spells/snipe.d
@@ -0,0 +1,25 @@
+import sorcerio.gameServer.spell;
+import sorcerio.gameServer.spells.base.projectile;
+import sorcerio.gameServer.player;
+
+class SnipeSpell : ProjectileSpell {
+	mixin registerSpell!(SpellName.snipe, 1300);
+
+	private enum ushort damage = 77;
+
+	override string humanReadableEffect() {
+		import std.conv;
+		return text("Does ", damage, " damage to affected players");
+	}
+
+	override void initialize() {
+		radius = 5;
+		range = 825;
+		speed = 3;
+		super.initialize();
+	}
+
+	override void affectPlayer(Player affectedPlayer) {
+		affectedPlayer.doRewardedDamage(damage, caster);
+	}
+}
